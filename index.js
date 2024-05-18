cd3e26cc56a5fa118d080e160ac0cff33815e069
Shiv Bhadaniya <shivbhadaniya56@gmail.com>
2024-05-18 18:16:36 +0530

    test: add healthcehck a test API

--- index.js
+++ index.js
@@ -1,48 +1,51 @@
 import express from "express";
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config();
 import cors from "cors";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
-import cookieParser  from 'cookie-parser'
+import cookieParser from "cookie-parser";
 
 import adminRouter from "./routes/Admin.js";
 import userRouter from "./routes/User.js";
 import authRoute from "./routes/Auth.js";
 import { Check } from "./util/envCheck.js";
 
-
-
 const app = express();
-app.use(cookieParser())
+app.use(cookieParser());
 
 const corsOption = {
-    corsOptions:true,
-    origin: true, //included origin as true
-    credentials: true, //included credentials as true
-}
+  corsOptions: true,
+  origin: true, //included origin as true
+  credentials: true, //included credentials as true
+};
 app.use(cors(corsOption));
 
-
-
-app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }))
-app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(bodyParser.json({ limit: "50mb" }));
 
 app.use("/", userRouter);
 app.use("/user/auth", authRoute);
 app.use("/admin", adminRouter);
 
-if(!Check.verifyEnvironment())  {
-    throw Error("Please include all environment variable");
+app.get("/healthcheck", (req, res) => {
+  res.status(200).send("Server is running");
+});
+
+if (!Check.verifyEnvironment()) {
+  throw Error("Please include all environment variable");
 }
 
 const PORT = process.env.PORT;
 const DB_CONNECTION_URL = process.env.DB_CONNECTION_URL;
 
-mongoose.connect(DB_CONNECTION_URL).then(() => {
+mongoose
+  .connect(DB_CONNECTION_URL)
+  .then(() => {
     app.listen(PORT, () => {
-        console.log(`Server running and Database connect successfully.`);
-    });   
-}).catch((err) => {
+      console.log(`Server running and Database connect successfully.`);
+    });
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
