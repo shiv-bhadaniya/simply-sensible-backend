e35067606521fcc92d5f6552f1fbe83fa37997d5
Shiv Bhadaniya <shivbhadaniya56@gmail.com>
2024-05-23 16:22:36 +0530

    fix: payment key issue

--- controller/Payment.js
+++ controller/Payment.js
@@ -3,28 +3,26 @@ const router = express.Router();
 
 import Stripe from "stripe";
 
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config();
 
-const stripe = Stripe(process.env.STRIPE_API_KEY)
+const stripe = Stripe(process.env.STRIPE_API_KEY);
 
 export const paymentProcess = async (req, res) => {
+  let myamount = req.body.amount;
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: myamount,
+    currency: "inr",
+    receipt_email: req.user.email,
+  });
 
-        let myamount = req.body.amount;
-        const paymentIntent = await stripe.paymentIntents.create({
-            amount: myamount,
-            currency: 'inr',
-            receipt_email: req.user.email,
-        });
-
-        res.status(200).json({ success: true, client_secret: paymentIntent.client_secret });
-}
+  res
+    .status(200)
+    .json({ success: true, client_secret: paymentIntent.client_secret });
+};
 
 export const sendStripAPIKey = async (req, res) => {
-
-
-    res.status(200).json({ stripeApiKey: process.env.STRIPE_API_KEY });
-
-}
+  res.status(200).json({ stripeApiKey: process.env.STRIPE_PUBLISHABLE_KEY });
+};
 
 export default router;
