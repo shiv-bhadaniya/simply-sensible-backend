f494a6f06bc4f8fb1f4e827f82191bc3811117e3
Shiv Bhadaniya <shivbhadaniya56@gmail.com>
2024-05-29 17:53:04 +0530

    config advance CORS

--- controller/Auth.js
+++ controller/Auth.js
@@ -5,100 +5,100 @@ import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-
 export const signin = async (req, res) => {
+  const { email, password } = req.body;
 
+  try {
+    const user = await User.findOne({ email });
 
-    const { email, password } = req.body;
+    if (!user) return res.status(400).json({ message: "user not found." });
 
-    try {
+    const checkPass = await bcrypt.compare(password, user.password);
 
-        const user = await User.findOne({email});
+    if (!checkPass)
+      return res.status(400).json({ message: "Password does not match. " });
 
-        if(!user) return res.status(400).json({message : "user not found."})
+    const token = jwt.sign(
+      { email: user.email, id: user._id, isAdmin: user.isAdmin },
+      process.env.secretKey,
+      { expiresIn: "2 days" }
+    );
 
-        const checkPass = await bcrypt.compare(password, user.password);
+    const sendingUserDetails = {
+      email: user.email,
+      isAdmin: user.isAdmin,
+      name: user.name,
+      userId: user._id,
+    };
 
-        if(!checkPass) return res.status(400).json({message : "Password does not match. "});
-        
-        const token = jwt.sign({email : user.email, id : user._id, isAdmin : user.isAdmin }, process.env.secretKey, { expiresIn: "2 days" } );        
-        
-        const sendingUserDetails = {
-            email : user.email,
-            isAdmin : user.isAdmin,
-            name : user.name,
-            userId: user._id,
-        }
+    const options = {
+      httpOnly: false,
+      // sameSite: "None",
+      // secure: true,
+    };
 
+    res.status(200).cookie("token", token, options).json({
+      result: sendingUserDetails,
+      token,
+    });
 
-
-        
-        const options = {
-            httpOnly: false,
-        };
-
-        
-        res.status(200).cookie("token", token, options).json({
-            result : sendingUserDetails, token
-        });
-        
-        console.log("Successfully signin. ");
-        
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message : "Something went wrong."});
-    }
-
-}
-
+    console.log("Successfully signin. ");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong." });
+  }
+};
 
 // signup new user
 export const signup = async (req, res) => {
-    
-    const { email, password, confirmPassword, name } = req.body;
-    try {
-        
-        const userExist = await User.findOne({email});
+  const { email, password, confirmPassword, name } = req.body;
+  try {
+    const userExist = await User.findOne({ email });
 
-        if(userExist) return res.status(400).json({message : "User already exist. "});
+    if (userExist)
+      return res.status(400).json({ message: "User already exist. " });
 
-        if(password !== confirmPassword) return res.status(400).json({message : "Password does not match. "});
-        
-        
-        const hasPass = await bcrypt.hash(password, 12);
-        
+    if (password !== confirmPassword)
+      return res.status(400).json({ message: "Password does not match. " });
 
-        var ans = email.search("@simplysensible.in");
-        var flag = (ans ==-1) ? false : true
-        
- 
+    const hasPass = await bcrypt.hash(password, 12);
 
-        const newUser = await User.create({email : email, password : hasPass, name: name, isAdmin: flag } );
-        console.log(newUser);
-        const token = jwt.sign({email : newUser.email, id : newUser._id, isAdmin : newUser.isAdmin }, process.env.secretKey, { expiresIn: "2 days" } );
+    var ans = email.search("@simplysensible.in");
+    var flag = ans == -1 ? false : true;
 
-        const sendingUserDetails = {
-            email : email,
-            isAdmin : newUser.isAdmin,
-            name : name,
-            userId: newUser._id,
-        }
+    const newUser = await User.create({
+      email: email,
+      password: hasPass,
+      name: name,
+      isAdmin: flag,
+    });
+    console.log(newUser);
+    const token = jwt.sign(
+      { email: newUser.email, id: newUser._id, isAdmin: newUser.isAdmin },
+      process.env.secretKey,
+      { expiresIn: "2 days" }
+    );
 
+    const sendingUserDetails = {
+      email: email,
+      isAdmin: newUser.isAdmin,
+      name: name,
+      userId: newUser._id,
+    };
 
-        const options = {
-            httpOnly: false,
-        };
+    const options = {
+      httpOnly: false,
+    };
 
-        
-        res.status(200).cookie("token", token, options).json({
-            result : sendingUserDetails, token
-        });
-        
-        console.log("Successfully signup. ");
+    res.status(200).cookie("token", token, options).json({
+      result: sendingUserDetails,
+      token,
+    });
 
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message : "Something went wrong."});
-    }
-}
+    console.log("Successfully signup. ");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong." });
+  }
+};
 export default router;
--- index.js
+++ index.js
@@ -17,9 +17,41 @@ app.use(cookieParser());
 const corsOptions = {
   origin: [process.env.FRONTEND_HOST, "http://localhost:3000"],
   credentials: true,
+  // methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  // allowedHeaders: ["Content-Type", "Authorization"],
+  // exposedHeaders: ["Set-Cookie"],
 };
 app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
+// app.use(express.json());
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", process.env.FRONTEND_HOST);
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   next();
+// });
+// app.use(function (req, res, next) {
+//   // Website you wish to allow to connect
+//   res.setHeader("Access-Control-Allow-Origin", [
+//     process.env.FRONTEND_HOST,
+//     "http://localhost:3000",
+//   ]);
+//   // Request methods you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
+//   // Request headers you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin,X-Requested-With,content-type,set-cookie"
+//   );
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader("Access-Control-Allow-Credentials", true);
 
+//   // Pass to next layer of middleware
+//   next();
+// });
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(bodyParser.json({ limit: "50mb" }));
 
