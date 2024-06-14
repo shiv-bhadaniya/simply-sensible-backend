25f3e43e6653005443697f36351262ced8243ce2
Shiv Bhadaniya <shivbhadaniya56@gmail.com>
2024-06-14 23:57:44 +0530

    refactor: handle stripe payment failure cases

--- controller/Payment.js
+++ controller/Payment.js
@@ -9,16 +9,36 @@ dotenv.config();
 const stripe = Stripe(process.env.STRIPE_API_KEY);
 
 export const paymentProcess = async (req, res) => {
-  let myamount = req.body.amount;
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: myamount,
-    currency: "inr",
-    receipt_email: req.user.email,
-  });
-
-  res
-    .status(200)
-    .json({ success: true, client_secret: paymentIntent.client_secret });
+  try {
+    let myamount = req.body.amount;
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: myamount,
+      currency: "inr",
+      receipt_email: req.user.email,
+    });
+    console.log("No error.");
+    res
+      .status(200)
+      .json({ success: true, client_secret: paymentIntent.client_secret });
+  } catch (e) {
+    switch (e.type) {
+      case "StripeCardError":
+        console.error("A payment error occurred: ", e.message);
+        break;
+      case "StripeInvalidRequestError":
+        console.error("An invalid request occurred.");
+        break;
+      case "ETIMEDOUT":
+      case "ENOTFOUND":
+        console.error("Network error: ", error.message);
+      case "ECONNREFUSED":
+        console.error("Connection refused : ", error.message);
+      default:
+        console.error("Another problem occurred, maybe unrelated to Stripe.");
+        break;
+    }
+    res.status(402).json({ success: false, message: e?.message });
+  }
 };
 
 export const sendStripAPIKey = async (req, res) => {
